2256ecdbcd0a7a752a10008860b2247c1d2fbac8
Daniel Fireman <danielfireman@gmail.com>
2018-07-31 12:22:33 -0300

    Actually tracking old and new objects

--- lib/gci.rb
+++ lib/gci.rb
@@ -1,14 +1,21 @@
 class Gci
 	def initialize app
 		@app = app
-		GC.disable	
+		GC.disable
+		puts "==< Automatic GC Disabled <=="	
 	end	
 
 	def call env
 		case env["HTTP_GCI"]
 			when "ch"
-				@s = GC.stat
-				[200, {}, [@s[:malloc_increase_bytes].to_s+"|"+@s[:oldmalloc_increase_bytes].to_s]]
+				# Some references.
+				# http://tmm1.net/
+				# https://www.speedshop.co/2017/03/09/a-guide-to-gc-stat.html
+				old_objects = GC.stat[:old_objects]
+				live_slots = GC.stat[:heap_live_slots]
+				gen1 = (live_slots - old_objects) * GC::INTERNAL_CONSTANTS[:RVALUE_SIZE]
+				gen2 = old_objects * GC::INTERNAL_CONSTANTS[:RVALUE_SIZE] 
+				[200, {}, [gen1.to_s+"|"+gen2.to_s]]
 
 			when "gen1"
 				GC.start(full_mark:false)
